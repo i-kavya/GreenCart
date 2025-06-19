d6882faa4564f7654605a25ca7f75ba18203af7e
Kavya Jaimini <kavyajaimini39@gmail.com>
2025-06-19 22:49:47 +0530

    Backend Part3

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { assets, categories } from '../../assets/assets';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const AddProduct = () => {
 
@@ -10,8 +12,40 @@ const AddProduct = () => {
   const [price, setPrice] = useState('');
   const [offerPrice, setOfferPrice] = useState('');
 
+  const {axios} = useAppContext()
+
   const onSubmitHandler = async(event)=>{
-    event.preventDefault();
+    try {
+        event.preventDefault();
+        const productData = {
+            name,
+            description: description.split('\n'),
+            category,
+            price,
+            offerPrice
+        }
+        const formData = new FormData();
+        formData.append('productData',JSON.stringify(productData));
+        for(let i=0;i<files.length;i++){
+            formData.append('images',files[i])
+        }
+        const {data} = await axios.post('/api/product/add',formData)
+        if(data.success){
+            toast.success(data.message);
+            setName('')
+            setDescription('')
+            setCategory('')
+            setPrice('')
+            setOfferPrice('')
+            setFiles([])
+        }
+        else{
+            toast.error(data.message)
+        }
+
+    } catch (error) {
+        toast.error(error.message)
+    }
   }
 
   return (
@@ -47,7 +81,7 @@ const AddProduct = () => {
                 </div>
                 <div className="w-full flex flex-col gap-1">
                     <label className="text-base font-medium" htmlFor="category">Category</label>
-                    <select onChange={(e)=>setName(e.target.value)} value={category}
+                    <select onChange={(e)=>setCategory(e.target.value)} value={category}
                     id="category" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40">
                         <option value="">Select Category</option>
                         {categories.map((item,index)=>(
